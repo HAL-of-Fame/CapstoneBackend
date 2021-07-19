dc449ade909b05fe7e4fa5d1c1d980878dcc0170
rrll3553 <liveoak35@gmail.com>
2021-07-19 10:31:16 -0700

    Functioning fetch all posts and fetch posts by ID routes and models

--- models/posts.js
+++ models/posts.js
@@ -1,20 +1,71 @@
 const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../utils/errors");
+
 class Post {
+  // list all posts in db in descending order of creation
   static async listPosts() {
-    // list all posts in db in descending order of creation
+    const results = await db.query(
+      `
+      SELECT p.id,
+             p.caption,
+             p.user_id AS "userId",
+             u.email AS "userEmail",
+             p.created_at AS "createdAt",
+             p.updateD_at AS "updatedAt"
+      FROM posts AS p
+      JOIN users AS u ON u.id = p.user_id
+      ORDER BY p.created_at DESC
+      `
+    );
+    return results.rows;
   }
 
-  static async fetchPostById({ postId }) {
-    // fetch a single post
+  // fetch a single post
+  static async fetchPostById(postId) {
+    const results = await db.query(
+      `
+    SELECT * FROM posts WHERE id= $1
+    `,
+      [postId]
+    );
+    const post = results.rows[0];
+    if (!post) {
+      throw new NotFoundError();
+    }
+    return post;
   }
 
+  // create a new post
   static async createNewPost({ post, user }) {
-    // create a new post
+    const requiredFields = ["caption"];
+    requiredFields.forEach((field) => {
+      if (!post.hasOwnProperty(field)) {
+        throw new BadRequestError(
+          `Required field - ${field} - missing from request body`
+        );
+      }
+    });
+
+    if (post.caption.length > 140) {
+      throw new BadRequestError(`Post caption must be 140 characters or less`);
+    }
+    const results = await db.query(
+      `
+      INSERT INTO posts (caption, user_id)
+      VALUES ($1, $2)
+      RETURNING id AS "primaryKey",
+                user_id,
+                caption,
+                created_at AS "createdAt",
+                updated_at AS "updatedAt"  
+                `,
+      [post.caption, user.id]
+    );
+    return results.rows[0];
   }
 
-  static async editPost({ postId, postUpdate }) {
-    // edit a new post
-  }
+  // edit a new post
+  static async editPost({ postId, postUpdate }) {}
 }
 
-module.exports = Post
+module.exports = Post;
--- routes/posts.js
+++ routes/posts.js
@@ -1,71 +1,68 @@
 /** Routes for authentication. */
 
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
-const security = require("../middleware/security") // for middleware
-const { createUserJwt } = require("../utils/tokens") // to generate JWT tokens
+const router = express.Router();
+const security = require("../middleware/security"); // for middleware
+const { createUserJwt } = require("../utils/tokens"); // to generate JWT tokens
+const Post = require("../models/posts");
 
-
-
-router.post("/", async function (req, res, next) {
-  try {
-      // create a new post
-    const user = await User.login(req.body)
-    const token = createUserJwt(user)
-    // const user = await User.authenticate(req.body)
-    return res.status(200).json({ user, token })
-  } catch (err) {
-    next(err)
+// create a new post
+router.post(
+  "/",
+  security.requireAuthenticatedUser,
+  async function (req, res, next) {
+    try {
+      const { user } = res.locals;
+      const post = await Post.createNewPost({ user, post: req.body });
+      return res.status(200).json({ post });
+    } catch (err) {
+      next(err);
+    }
   }
-})
+);
 
+// fetch all posts
 router.get("/", async function (req, res, next) {
-    try {
-        // fetch all posts
-      const user = await User.login(req.body)
-      const token = createUserJwt(user)
-      // const user = await User.authenticate(req.body)
-      return res.status(200).json({ user, token })
-    } catch (err) {
-      next(err)
-    }
-  })
+  try {
+    const posts = await Post.listPosts();
+    return res.status(200).json({ posts });
+  } catch (err) {
+    next(err);
+  }
+});
 
-  router.get("/:postId", async function (req, res, next) {
-    try {
-        // fetch single post
-      const user = await User.login(req.body)
-      const token = createUserJwt(user)
-      // const user = await User.authenticate(req.body)
-      return res.status(200).json({ user, token })
-    } catch (err) {
-      next(err)
-    }
-  })
+// fetch single post
+router.get("/:postId", async function (req, res, next) {
+  try {
+    const { postId } = req.params;
+    const post = await Post.fetchPostById(postId);
+    return res.status(200).json({ post });
+  } catch (err) {
+    next(err);
+  }
+});
 
-  router.put("/:postId", async function (req, res, next) {
-    try {
-        // uodate a post 
-      const user = await User.login(req.body)
-      const token = createUserJwt(user)
-      // const user = await User.authenticate(req.body)
-      return res.status(200).json({ user, token })
-    } catch (err) {
-      next(err)
-    }
-  })
+// update a post
+router.put("/:postId", async function (req, res, next) {
+  try {
+    const user = await User.login(req.body);
+    const token = createUserJwt(user);
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    next(err);
+  }
+});
 
-
-  router.post("/:postId/ratings", async function (req, res, next) {
-    try {
-        // create a rating for a post
-      const user = await User.login(req.body)
-      const token = createUserJwt(user)
-      // const user = await User.authenticate(req.body)
-      return res.status(200).json({ user, token })
-    } catch (err) {
-      next(err)
-    }
-  })
-module.exports = router
+router.post("/:postId/ratings", async function (req, res, next) {
+  try {
+    // create a rating for a post
+    const user = await User.login(req.body);
+    const token = createUserJwt(user);
+    // const user = await User.authenticate(req.body)
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    next(err);
+  }
+});
+module.exports = router;

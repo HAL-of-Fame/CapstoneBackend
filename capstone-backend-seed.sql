3d00ae45bd61fc9a0a8fbea6f5291fece251deb8
lriveracastro407 <lriveracastro407@berkeley.edu>
2021-07-21 14:23:02 -0700

    order connected to front

--- capstone-backend-seed.sql
+++ capstone-backend-seed.sql
@@ -1,70 +1,38 @@
 -- Seed database with default products
 
 INSERT INTO products (name, image, description, price)
-VALUES ('Rice Krispies',
-  'https://upload.wikimedia.org/wikipedia/commons/c/cd/RKTsquares.jpg',
-  'Delicious corn-based rice grains melted together with marshmallows into a square-like shape.',
-  99
-  ), ('Flamin Hot Cheetos',
-  'https://static.openfoodfacts.org/images/products/896/400/009/0879/front_en.14.full.jpg',
-  'No one knows exactly what is in the powder that covers these snacks, but wow is it amazing!',
-  150
-), ('Cinnamon Rolls',
-  'https://upload.wikimedia.org/wikipedia/commons/7/7a/Johns_Inc_Cinnamon_Rolls.jpg',
-  'Basically just cake for breakfast, but whos complaining?',
-  299
-), ('Coconut Water',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/ONE_Pure_Coconut_Water.jpeg/900px-ONE_Pure_Coconut_Water.jpeg',
-  'Somehow incredibly hydrating, while also not really being water.',
-  325  
-), ('Fruit Medley',
-  'https://upload.wikimedia.org/wikipedia/commons/6/6e/Fruits.png',
-  'More fruit than you know what to do with!',
-  750
-), ('Bar Code Shirts',
-  'https://upload.wikimedia.org/wikipedia/commons/0/0f/Barcode_wikipedia_shirt.svg',
-  'So you can scan yourself in the checkout aisle.',
-  1999
-), ('Striped Socks',
-  'https://upload.wikimedia.org/wikipedia/commons/e/ed/Bulk_tube_socks.png',
-  'Because the dryer ate most of your socks and it never hurts to have another clean pair.',
-  499
-), ('Knitted Beanie',
-  'https://upload.wikimedia.org/wikipedia/commons/b/b6/Blue_knit-cap_beanie_by_RobertLender.jpeg',
-  'Keeps your head absurdly warm while the rest of your body freezes.',
-  1999
-), ('Pullover Sweater',
-  'https://upload.wikimedia.org/wikipedia/commons/f/fe/Jumper_altered_PsCSJPG10.jpg',
-  'At $14.99, can you really afford NOT to buy this?',
-  1499
-), ('Alpaca Wool Scarf',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Alpaca_wool_scarf.JPG/800px-Alpaca_wool_scarf.JPG',
-  'Legends say that the Alpaca knitted this scarf from its own fur.',
-  3499
-), ('Matching Rings',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Wedding_rings.jpg/800px-Wedding_rings.jpg',
-  'Ball out and get one for each finger!',
-  20000
-), ('Apple Watch',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Apple_Watch.jpg/800px-Apple_Watch.jpg',
-  'Why check your phone all the time when you can check your watch all the time?',
-  39999
-), ('Copper Bracelet',
-  'https://upload.wikimedia.org/wikipedia/commons/0/03/Copper_bracelet_04.jpg',
-  'Compliment this accessory with a batter and some wire and you have yourself a portable charger right on your wrist!',
-  7000
-), ('Keyboard',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Apple-wireless-keyboard.jpg/800px-Apple-wireless-keyboard.jpg',
-  'A wireless keyboard for your computer! Because who doesn''t like walking around while they type up an essay?',
-  11000
-), ('Mouse from 1998',
-  'https://upload.wikimedia.org/wikipedia/commons/a/a2/Mouse_Microsoft_PS2.jpg',
-  'Check out this fancy Microsoft mouse that was in use right before the dot com bubble crash of the 2000s! It''s a collectors'' item that still works great. Brings back that "on the verge of the world collapsing" feeling.',
-  1998
-), ('Smart Glasses',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Google_Glass_photo.JPG/330px-Google_Glass_photo.JPG',
-  'Have the internet mounted to your face, and get the added benefit of looking silly all the time!',
-  99999
+VALUES ('T-shirt',
+  'https://images.unsplash.com/photo-1620799139507-2a76f79a2f4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1932&q=80',
+  'Shirt',
+  10
+  ), ('Baseball Cap',
+  'https://images.unsplash.com/photo-1588850561407-ed78c282e89b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80',
+  'Baseball Cap',
+  10
+), ('Water Bottle',
+  'https://images.unsplash.com/photo-1602143407151-7111542de6e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
+  'Wata bot le',
+  25
+), ('Mug',
+  'https://images.unsplash.com/photo-1530968831187-a937ade474cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1850&q=80',
+  'Mug',
+  20  
+), ('Backpack',
+  'https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+  'back pack',
+  50
+), ('Pen',
+  'https://images.unsplash.com/photo-1583485088034-697b5bc54ccd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1846&q=80',
+  'Writing Utensil',
+  3
+), ('Phone Case',
+  'https://images.unsplash.com/photo-1547658718-f4311ad64746?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1900&q=80',
+  'Case para un telefono',
+  10
+), ('Notebook',
+  'https://images.unsplash.com/photo-1601001435957-74f0958a93fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80',
+  'Para las notas.',
+  16
 );
 
 

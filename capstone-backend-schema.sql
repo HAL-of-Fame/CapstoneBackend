b5a7a079e58c6ec0bc92b6109dd4c6da982fd04f
rrll3553 <liveoak35@gmail.com>
2021-07-28 16:46:50 -0700

    Changed genreOptions to capitalcase to match frontend

--- capstone-backend-schema.sql
+++ capstone-backend-schema.sql
@@ -8,11 +8,12 @@ CREATE TABLE users (
     is_admin    BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TYPE genreOptions AS ENUM ('action', 'comedy', 'romance', 'drama', 'science-fiction', 'horror');
+CREATE TYPE genreOptions AS ENUM ('Action', 'Animation', 'Comedy', 'Family', 'Fantasy', 'Romance', 'Drama', 'Science-Fiction', 'Horror', 'Thriller');
 CREATE TABLE posts (
   id          SERIAL PRIMARY KEY,
   title       VARCHAR(140) NOT NULL,
   genre       genreOptions,
+  movieName   TEXT DEFAULT NULL,
   text        TEXT NOT NULL,     
   user_id     INTEGER NOT NULL,
   created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
